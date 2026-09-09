5f4ce1bbce9a4ec52e0ba1c2a4d9be29e7c63115
dohajda-arch <do.hajda@gmail.com>
2026-09-09 23:26:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='platby-edennium-v7';
-const FILES=['./','./index.html','./manifest.webmanifest','./sw.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('fetch',()=>{});
