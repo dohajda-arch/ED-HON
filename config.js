e5a6b21aad8fe1c0937831e424eb42c3869dcbe4
dohajda-arch <do.hajda@gmail.com>
2026-09-10 02:51:15 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// SUPABASE CONFIGURATION
+window.EDENNIUM_CONFIG = {
+  SUPABASE_URL: "https://prlbahgvgihtdfwfikrp.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_uKx6LbKs2UbsdnJgugv21g_NknJYtLZ"
+};
